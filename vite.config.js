c7a3be3a4546c7e5db448079b1e392d9398cdb1a
webkubor <webkubor@163.com>
2024-11-13 10:30:43 +0800

    feat  暂时提交

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   plugins: [vue()],
 })
